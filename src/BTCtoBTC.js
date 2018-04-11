bbf9848513bdd73275508cd679395159e7aa9f36
jindalkushagra15 <coder@primetrade.ai>
2018-04-11 00:09:04 -0700

    BTC commit

--- src/BTCtoBTC.js
+++ src/BTCtoBTC.js
@@ -1,38 +1,57 @@
-let request = require("request")
+function getAnswer(){
 
-let relativeURL = "https://bittrex.com/api/v1.1/public/getmarkets"
+	let request = require("request")
+	let relativeURL = "https://bittrex.com/api/v1.1/public/getmarkets"
+	
+	request({
+    	url: relativeURL,
+    	json: true
+	}, async function (error, response, body) {
 
-request({
-    url: relativeURL,
-    json: true
-}, async function (error, response, body) {
+    	if (!error && response.statusCode === 200) {
 
-    if (!error && response.statusCode === 200) {
+        	let a=body;
+        	console.log(a);
 
-        let a=body;
-        console.log(a);
-
-    }
-    else{
-        console.log("error occured");
-    }
-})
+    	}
+    	else{
+        	console.log("error occured");
+    	}
+	})
 
 
-let MarketSummaryRelativeURL="https://bittrex.com/api/v1.1/public/getmarketsummaries"
+	let MarketSummaryRelativeURL="https://bittrex.com/api/v1.1/public/getmarketsummaries"
 
-request({
-    url: MarketSummaryRelativeURL,
-    json: true
-}, async function (error, response, body) {
+	request({
+    	url: MarketSummaryRelativeURL,
+    	json: true
+	}, async function (error, response, body) {
 
-      if (!error && response.statusCode === 200) {
+      	if (!error && response.statusCode === 200) {
 
-          let b=body;
-          console.log(b);
+          	let b=body;
+          	console.log(b);
 
-      }
-      else{
-          console.log("error occured");
-      }
- })
+      	}
+      	else{
+          	console.log("error occured");
+      	}
+ 	})
+
+}
+
+function getCurrency(jsonArray, btc, eth, keyBaseCurrency, KeyMarketCurrency){
+	for(i = 0;i < jsonArray.length;i++){
+		temp = i;
+		baseCurrency = temp.keyBaseCurrency;
+		marketCurrency = temp.KeyMarketCurrency;
+		if(baseCurrency == ("BTC")){
+			btc.push(marketCurrency);
+		}
+		else if(baseCurrency == ("ETH")){
+			eth.push(marketCurrency);
+		}
+	}
+	console.log(btc);
+}
+
