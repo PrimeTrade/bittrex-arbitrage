3ea857991d3ec2531452b7067f46c63d4d9df5cc
jindalkushagra15 <jindalkushagra15@stu.upes.ac.in>
2018-04-10 18:41:49 -0700

    initial commit

--- src/BTCtoBTC.js
+++ src/BTCtoBTC.js
@@ -1,15 +1,15 @@
-var request = require("request")
+let request = require("request")
 
-var relativeURL = "https://bittrex.com/api/v1.1/public/getmarkets"
+let relativeURL = "https://bittrex.com/api/v1.1/public/getmarkets"
 
 request({
     url: relativeURL,
     json: true
-}, function (error, response, body) {
+}, async function (error, response, body) {
 
     if (!error && response.statusCode === 200) {
 
-        var a=body;
+        let a=body;
         console.log(a);
 
     }
@@ -18,16 +18,17 @@ request({
     }
 })
 
-var MarketSummaryRelativeURL="https://bittrex.com/api/v1.1/public/getmarketsummaries"
+
+let MarketSummaryRelativeURL="https://bittrex.com/api/v1.1/public/getmarketsummaries"
 
 request({
     url: MarketSummaryRelativeURL,
     json: true
-}, function (error, response, body) {
+}, async function (error, response, body) {
 
       if (!error && response.statusCode === 200) {
 
-          var b=body;
+          let b=body;
           console.log(b);
 
       }
