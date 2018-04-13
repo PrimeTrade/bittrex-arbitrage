1cdb61f67252ed567f236e6a710e77e7b3fac4bd
jindalkushagra15 <coder@primetrade.ai>
2018-04-13 00:17:34 -0700

    Commit for BTC to get json array

--- src/BTCtoBTC.js
+++ src/BTCtoBTC.js
@@ -1,82 +1,84 @@
+let request = require("request")
+//let instance = require('./MarketAskName.js');
+
+let a,b;
+
 function getAnswer(){
 
-	let request = require("request")
-	let instance = require('./MarketAskName.js');
-	let a,b;
-	
-	let relativeURL = "https://bittrex.com/api/v1.1/public/getmarkets"
-	
-	request({
-    	url: relativeURL,
-    	json: true
-	}, async function (error, response, body) {
+    let relativeURL = "https://bittrex.com/api/v1.1/public/getmarkets";
 
-    	if (!error && response.statusCode === 200) {
+    request({
+        url: relativeURL,
+        json: true
+    }, function (error, response, body) {
 
-        	a=body;
-        	//console.log(a);
+        if (!error && response.statusCode === 200) {
 
-    	}
-    	else{
-        	console.log("error occured");
-    	}
-	})
+            a=body.result;
 
+            let MarketSummaryRelativeURL="https://bittrex.com/api/v1.1/public/getmarketsummaries"
 
-	let MarketSummaryRelativeURL="https://bittrex.com/api/v1.1/public/getmarketsummaries"
+            request({
+                url: MarketSummaryRelativeURL,
+                json: true
+            }, function (error, response, body) {
 
-	request({
-    	url: MarketSummaryRelativeURL,
-    	json: true
-	}, async function (error, response, body) {
+                if (!error && response.statusCode === 200) {
 
-      	if (!error && response.statusCode === 200) {
+                    b=body.result;
+                }
+                else{
+                    console.log("error occured");
+                }
+            });
 
-          	b=body;
-          	//console.log(b);
+            let btc = [];
+            let eth = [];
+            let KEY_BASE_CURRENCY = "BaseCurrency";
+            let KEY_MARKET_CURRENCY = "MarketCurrency";
 
-      	}
-      	else{
-          	console.log("error occured");
-      	}
- 	})
+            getCurrency(a, btc, eth, KEY_BASE_CURRENCY, KEY_MARKET_CURRENCY);
 
-	let btc = [];
-	let eth = [];
-	let KEY_BASE_CURRENCY = "BaseCurrency";
-    let KEY_MARKET_CURRENCY = "MarketCurrency";
-    
-	getCurrency(a, btc, eth, KEY_BASE_CURRENCY, KEY_MARKET_CURRENCY);
-	
-	let marketAskNames = [];
-	let btcName,etcName;
-	
-	for(btcName in btc){
-		for(etcName in eth){
-			//let a=new instance();
-		}
-	} 
-	
+            let marketAskNames = [];
+            let btcName,etcName;
+
+            for(btcName in btc){
+                for(etcName in eth){
+                    //let a=new instance();
+                }
+            }
+
+        }
+        else{
+            console.log("error occured");
+        }
+
+    })
 }
+
+
 
 function getCurrency(jsonArray, btc, eth, keyBaseCurrency, KeyMarketCurrency){
-	for(i = 0;i < Object.keys(jsonArray.result).length;i++){
-		temp = i;
-		baseCurrency = temp.keyBaseCurrency;
-		marketCurrency = temp.KeyMarketCurrency;
-		if(baseCurrency == ("BTC")){
-			btc.push(marketCurrency);
-		}
-		else if(baseCurrency == ("ETH")){
-			eth.push(marketCurrency);
-		}
-	}
-	console.log(btc);
+
+    for(i = 0;i<jsonArray.length;i++){
+        temp = jsonArray[i];
+        console.log(temp);
+        baseCurrency = temp.BaseCurrency;
+        marketCurrency = temp.MarketCurrency;
+
+        console.log(baseCurrency);
+        console.log(marketCurrency);
+
+        if(baseCurrency == ("BTC")){
+            btc.push(marketCurrency);
+        }
+        else if(baseCurrency == ("ETH")){
+            eth.push(marketCurrency);
+        }
+    }
+    console.log(btc);
 }
-   
-   
-   
-   
-   getAnswer();
+
+getAnswer();
 
 
