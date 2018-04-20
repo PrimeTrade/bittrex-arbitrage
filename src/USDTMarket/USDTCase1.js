1d434d4ea54b2fc59073368ffada7b1ce61a7497
primetrade-coder <38075928+primetrade-coder@users.noreply.github.com>
2018-04-20 13:17:10 -0700

    To get ETH-USDT Markets

--- src/USDTMarket/USDTCase1.js
+++ src/USDTMarket/USDTCase1.js
@@ -1,17 +1,43 @@
 let https=require('https');
+console.log("1 Passed");
 let URL=require('url').URL;
-const BASE_URL=new URL('https://bittrex.com/api/v1.1');
+let jsonObject;
+let jsonArray=[];
+let MSObject;
+let MSArray=[];
+const BASE_URL=new URL('https://bittrex.com/api/v1.1/');
 const KEY_BASE_CURRENCY='BaseCurrency';
 const KEY_MARKET_CURRENCY='MarketCurrency';
+const KEY_MARKET_NAME="MarketName";
+const KEY_ASK="Ask";
+const KEY_BID="Bid";
 let getAnswer= function(){
-    let jsonObject,MSObject,jsonArray,MSArray;
-    const relativeUrl=new URL('/public/getmarkets',BASE_URL);
-    let req=https.request(relativeUrl,(res)=>{
+    console.log("2 Passed");
+    let relativeUrl=new URL('public/getmarkets',BASE_URL);
+    let req=https.get(relativeUrl,(res)=>{
         console.log(`Status:${res.statusCode}`);
         res.on('data',(chunk)=>{
             jsonObject=JSON.stringify(chunk);
             jsonArray=JSON.parse(jsonObject);
-            console.log(`Body: ${jsonArray}`);
+            console.log(`Markets: ${jsonArray.data}`);
+        });
+        res.on('end',()=>{
+            console.log("No more data");
+        });
+    });
+    req.on('error',(e)=>{
+        console.log(e);
+    });
+    let usdt=[];
+    let eth=[];
+    //req.abort();
+    const MarketSummaryRelativeURL=new URL('/public/getmarketsummaries',BASE_URL);
+    req=https.get(MarketSummaryRelativeURL,(res)=>{
+        console.log(`Status:${res.statusCode}`);
+        res.on('data',(chunk)=>{
+            MSObject=JSON.stringify(chunk);
+            MSArray=JSON.parse(MSObject);
+            console.log(`MarketSummaries:${MSArray.data}`);
         });
         res.on('end',()=>{
             console.log('No more data');
@@ -20,32 +46,31 @@ let getAnswer= function(){
     req.on('error',(e)=>{
         console.log(e);
     });
-    let usdt=[];
-    let eth=[];
-    req.abort();
-    const MarketSummaryRelativeURL=new URL('/public/getmarketsummaries',BASE_URL);
-    req=https.request(MarketSummaryRelativeURL,(res)=>{
-        console.log(`Status:${res.statusCode}`);
-        res.on('data',(chunk)=>{
-            MSObject=JSON.stringify(chunk);
-            MSArray=JSON.parse(MSObject);
-            console.log(`MarketSummaries:${MSObject}`);
-        });
-        res.on('end',()=>{
-            console.log('No more data');
-        });
-    });
-    req.on('error',(e)=>{
-        console,log(e);
-    });
-    req.abort();
+    //req.abort();
+    console.log("3 Passed");
+    console.log(jsonArray);
     getUSDTETHMarkets(jsonArray, usdt, eth, KEY_BASE_CURRENCY, KEY_MARKET_CURRENCY);
     let marketAskNames=[];
+    let etcName,usdtName;
+    for(etcName in eth){
+        for(usdtName in usdt){
+            if(getAskValue(MSArray,marketAskNames,etcName,usdtName,KEY_MARKET_NAME,KEY_ASK,KEY_BID))
+                break;
+        }
+    }
 }
 let getUSDTETHMarkets=function(jsonArray,usdt,eth,keyBaseCurrency,keyMarketCurrency){
-    for(let i=0;i<jsonArray.length();i++){
+    let temp,baseCurrency,marketCurrency;
+    for(let i=0;i<jsonArray.length;i++){
         temp=jsonArray[i];
-
+        baseCurrency=temp.keyBaseCurrency;
+        marketCurrency=temp.keyMarketCurrency;
+        if(baseCurrency===("USDT")){
+            usdt.push(marketCurrency);
+        }
+        else if(baseCurrency===("ETH")){
+            eth.push(marketCurrency);
+        }
 
     }
 }
